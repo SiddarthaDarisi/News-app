1f19c055a6625333a497dc95bd735b75cbccb502
SiddarthaDarisi <90901828+SiddarthaDarisi@users.noreply.github.com>
2023-03-18 21:28:41 -0400

    news

--- src/Landing.js
+++ src/Landing.js
@@ -2,21 +2,24 @@
 //navbar with login on right side and "news-app" text on left side
 import React from 'react';
 import { Link } from "react-router-dom";
-
+import News from './news.js';
 function Landing() {
     return (
         <div className="Landing">
-        <nav>
-            <ul>
-            <li>
-                <Link to="/">News-App</Link>
-            </li>
-            <li>
-                <Link to="/login">Login</Link>
-            </li>
-            </ul>
-        </nav>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">News-App</Link>
+                    </li>
+                    <li>
+                        <Link to="/login">Login</Link>
+                    </li>
+                </ul>
+            </nav>
+            <News />
         </div>
+
+
     );
-    }
+}
 export default Landing;
