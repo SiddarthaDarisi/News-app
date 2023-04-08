adde1693df7297dbb24fdbc0f7741844f1e547c7
SiddarthaDarisi <90901828+SiddarthaDarisi@users.noreply.github.com>
2023-04-08 15:06:23 -0400

    login test

--- src/login.test.js
+++ src/login.test.js
@@ -1,8 +1,17 @@
 import { Auth } from "aws-amplify";
-describe('test news_app', function () {
-  it('test news-app.Login', function (done) {
-    let result = news_app.Login();
-    assert.equal(result, 1);
-    done();
-  })
-})
+import config from './aws-exports';
+import { Amplify } from 'aws-amplify'
+
+Amplify.configure(config);
+
+describe('Auth', () => {
+  it('should sign in a user', async () => {
+    const username = 'siddarthadarisi@gmail.com';
+    const password = 'Cvr7894@';
+
+    const result = await Auth.signIn(username, password);
+
+    expect(result).toBeDefined();
+    expect(result.username).toEqual("25bff222-ecb6-4011-8c1c-643c12ea6fd5");
+  });
+});
