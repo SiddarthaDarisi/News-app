71d74463f3c747455ee169d8345137e8cce5541f
SiddarthaDarisi <90901828+SiddarthaDarisi@users.noreply.github.com>
2023-04-10 17:13:48 -0400

    Fixed news.js

--- src/news.js
+++ src/news.js
@@ -1,20 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Grid, Card, Box, CardContent, CardMedia, Typography, Pagination } from '@mui/material';
+import { Grid, Card, CardContent, CardMedia, Typography, Pagination } from '@mui/material';
+
 function News({ categories }) {
     const [newsData, setNewsData] = useState([]);
     const [loading, setLoading] = useState(false);
     const [page, setPage] = useState(1);
     const [totalPages, setTotalPages] = useState(1);
-    const [maxPage, setMaxPage] = useState(5);
+    const articlesPerPage = 10;
+
     async function getNewsData() {
         setLoading(true);
         console.log(`News: ${categories}`);
         try {
             const numCategories = categories.length;
-            const numArticlesPerCategory = Math.ceil(50 / numCategories);
+            let divider = 100;
+            if(numCategories === 2) {
+                divider = 200;
+            } else if(numCategories > 2) {
+                divider = 250;
+            }
+            const numArticlesPerCategory = Math.floor( divider / numCategories);
             let promises = categories.map((category) => axios.get(
-                `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=d1165b675f3b450a9879de4ff8997ac9&pageSize=${numArticlesPerCategory}&page=${page}`
+                `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=e22725d642174ef0b0f1e1d3ec449714&pageSize=${numArticlesPerCategory}`
             ));
             const resps = await Promise.all(promises);
             const articles = resps.reduce((accumulator, currentValue) => {
@@ -23,22 +31,24 @@ function News({ categories }) {
             }, []);
             const sortedArticles = articles.sort((a, b) => new Date(b.publishedAt) - new Date(a.publishedAt));
             setNewsData(sortedArticles);
-            setTotalPages(Math.ceil(sortedArticles.length / 20));
+            setTotalPages(Math.ceil(sortedArticles.length / articlesPerPage));
             setLoading(false);
         } catch (error) {
             console.error(error);
             setLoading(false);
         }
     }
+
     useEffect(() => {
         getNewsData();
-    }, [categories, page]);
+    }, [categories]);
 
     const handlePageChange = (event, value) => {
-        if (value >= 1 && value <= maxPage) {
-            setPage(value);
-        }
+        setPage(value);
     };
+
+    const start = (page - 1) * articlesPerPage;
+    const end = start + articlesPerPage;
 
     return (
         <>
@@ -46,7 +56,7 @@ function News({ categories }) {
                 {loading ? (
                     <Typography variant="h5">Loading...</Typography>
                 ) : (
-                    newsData.map((newsData, index) => (
+                    newsData.slice(start, end).map((newsData, index) => (
                         <Grid item xs={12} sm={6} md={4} key={index}>
                             <Card sx={{ position: 'relative' }}>
   <CardMedia component="img" height="140" image={newsData.urlToImage} alt="news" />
@@ -68,7 +78,7 @@ function News({ categories }) {
                 )}
             </Grid>
             <div style={{ display: 'flex', justifyContent: 'center' }}>
-                <Pagination mt={10} count={Math.min(totalPages, maxPage)} page={page} onChange={handlePageChange} />
+                <Pagination mt={10} count={totalPages} page={page} onChange={handlePageChange} />
             </div>
         </>
     );
