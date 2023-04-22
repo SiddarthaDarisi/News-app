787f0306adcd443f03ed9e9091acbaa32aef70c4
SiddarthaDarisi <90901828+SiddarthaDarisi@users.noreply.github.com>
2023-04-22 11:34:14 -0400

    search bar unit tests passed

--- src/news2.test.js
+++ src/news2.test.js
@@ -1,61 +1,52 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import axios from 'axios';
-import News from './News';
-jest.mock('axios');
-describe('News component', () => {
-    afterEach(() => {
-        jest.clearAllMocks();
+import News from './news';
+import { API } from 'aws-amplify';
+// Mock API response
+const mockNewsData = [
+    {
+        id: 1,
+        title: "Test Article 1",
+        description: "Test article 1 description",
+    },
+    {
+        id: 2,
+        title: "Test Article 2",
+        description: "Test article 2 description",
+    },
+];
+
+jest.mock("../");
+
+describe("News", () => {
+    test("renders news articles", async () => {
+        API.post.mockResolvedValue({ data: { articles: mockNewsData } });
+
+        render(<News />);
+
+        // Wait for the loading state to disappear
+        await waitFor(() => expect(screen.queryByText("Loading...")).toBeNull());
+
+        // Check for expected content
+        expect(screen.getByText("Test Article 1")).toBeInTheDocument();
+        expect(screen.getByText("Test article 1 description")).toBeInTheDocument();
+        expect(screen.getByText("Test Article 2")).toBeInTheDocument();
+        expect(screen.getByText("Test article 2 description")).toBeInTheDocument();
     });
-
-    test('renders News component with loading indicator', async () => {
-        const categories = ['general'];
-        axios.get.mockResolvedValue({});
-
-        render(<News categories={categories} />);
+    it('renders loading state', () => {
+        render(<News categories={[]} />);
 
         expect(screen.getByText('Loading...')).toBeInTheDocument();
     });
 
-    test('renders News component with fetched news data', async () => {
-        const categories = ['general'];
-        const sampleData = {
-            data: {
-                articles: [
-                    {
-                        title: 'Sample News',
-                        description: 'Sample news description',
-                        urlToImage: 'http://example.com/sample-image.jpg',
-                        publishedAt: '2023-01-01T12:00:00Z',
-                    },
-                ],
-            },
-        };
+    it('renders error message if search fails', async () => {
+        API.post.mockResolvedValue([]);
 
-        axios.get.mockResolvedValue(sampleData);
+        render(<News categories={[]} searchQuery="no results" />);
 
-        render(<News categories={categories} />);
+        await waitFor(() => expect(API.post).toHaveBeenCalled());
 
-        await waitFor(() => screen.getByText('Sample News'));
-
-        expect(screen.getByText('Sample News')).toBeInTheDocument();
-        expect(screen.getByText('Sample news description')).toBeInTheDocument();
-        expect(screen.getByAltText('news')).toBeInTheDocument();
-        expect(screen.getByText('2023-01-01T12:00:00Z')).toBeInTheDocument();
-    });
-
-
-    test('calls axios.get with correct category and page number', async () => {
-        const categories = ['general'];
-        axios.get.mockResolvedValue({});
-
-        render(<News categories={categories} />);
-
-        await waitFor(() => axios.get);
-
-        expect(axios.get).toHaveBeenCalledWith(
-            'https://newsapi.org/v2/top-headlines?country=us&category=general&apiKey=d1165b675f3b450a9879de4ff8997ac9&pageSize=50&page=1'
-        );
+        expect(screen.getByText('Your search - no results - did not match any documents.')).toBeInTheDocument();
     });
 });
--- src/searchbar.test.js
+++ src/searchbar.test.js
@@ -1,44 +1,22 @@
-import Dashboard from "./Dashboard";
-describe('Dashboard search bar', () => {
-    let searchBar;
-  
-    beforeEach(() => {
-      searchBar = new SearchBar();
-      // Render the search bar component
-      searchBar.render();
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import SearchBar from './SearchBar';
+
+describe('SearchBar', () => {
+    test('renders search bar correctly', () => {
+        render(<SearchBar onSearch={() => {}} />);
+        expect(screen.getByPlaceholderText('Search...')).toBeInTheDocument();
     });
-  
-    afterEach(() => {
-      // Destroy the search bar component
-      searchBar.destroy();
+
+    test('triggers search on Enter key press', () => {
+        const handleSearch = jest.fn();
+        render(<SearchBar onSearch={handleSearch} />);
+        const input = screen.getByPlaceholderText('Search...');
+
+        fireEvent.keyDown(input, { key: 'Enter' });
+        expect(handleSearch).toHaveBeenCalled();
     });
-  
-    it('should update the search query when the user types', () => {
-      // Type a query into the search bar
-      searchBar.typeQuery('example query');
-      // Expect the search bar's query to be updated
-      expect(searchBar.getQuery()).toBe('example query');
-    });
-  
-    it('should call the search function when the user submits a query', () => {
-      // Mock the search function
-      const mockSearch = jest.fn();
-      // Subscribe to the search event
-      searchBar.onSearch(mockSearch);
-      // Type a query into the search bar
-      searchBar.typeQuery('example query');
-      // Submit the query
-      searchBar.submitQuery();
-      // Expect the search function to be called with the query as an argument
-      expect(mockSearch).toHaveBeenCalledWith('example query');
-    });
-  
-    it('should clear the search query when the user clicks the clear button', () => {
-      // Type a query into the search bar
-      searchBar.typeQuery('example query');
-      // Click the clear button
-      searchBar.clickClear();
-      // Expect the search bar's query to be empty
-      expect(searchBar.getQuery()).toBe('');
-    });
-  });
+
+   
+});
